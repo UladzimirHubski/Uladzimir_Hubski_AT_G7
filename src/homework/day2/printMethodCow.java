7c135afa469e1b782785a3a0c447f5f23acc9436
Uladzimir Hubski <vladimirgubskij@gmail.com>
2023-02-21 21:26:12 +0300

    Finished printMethodCow task

---
+++ src/homework/day2/printMethodCow.java
@@ -0,0 +1,16 @@
+package homework.day2;
+
+public class printMethodCow {
+
+    public static void main(String[] args) {
+
+        System.out.print("\t   ^__^\n" +
+                "\t  (oo)\\\\_______\n" +
+                "\t (__)\\ ) \\/\\  \\\n" +
+                "\t      ||----w |\\\n" +
+                "\t      ||     ||");
+
+
+    }
+
+}
