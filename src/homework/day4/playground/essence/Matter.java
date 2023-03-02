ce346f7244f553a0c3038ecfa2ec2a764555dbac
Uladzimir Hubski <vladimirgubskij@gmail.com>
2023-03-02 11:07:41 +0300

    Hometask Essence part2

--- src/homework/day4/playground/essence/Matter.java
+++ src/homework/day4/playground/essence/Matter.java
@@ -1,4 +1,14 @@
 package homework.day4.playground.essence;
 
 public abstract class Matter {
+    protected int mass;
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
 }
--- src/homework/day4/playground/essence/creatures/Insects.java
+++ src/homework/day4/playground/essence/creatures/Insects.java
@@ -1,4 +1,14 @@
 package homework.day4.playground.essence.creatures;
 
 public class Insects extends Animal {
+    protected String nameInsects;
+
+    public String getNameInsects() {
+        return nameInsects;
+    }
+
+
+    public void setNameInsects(String nameInsects) {
+        this.nameInsects = nameInsects;
+    }
 }
