bf532dbbf9dc1e5b80ac8fef362f692f6fdbf5b5
Uladzimir Hubski <vladimirgubskij@gmail.com>
2023-03-11 13:07:33 +0300

    Hometask Playground essence 7 part

--- src/homework/day5/sorting/Task1_3.java
+++ src/homework/day5/sorting/Task1_3.java
@@ -1,14 +1,14 @@
 package homework.day5.sorting;
-//tbd
-//public class allInts {
-//    public void printInts(String text) {
-//        char[] array = text.toCharArray();
-//        String number;
-//        for (int i = 0; i < array.length; i++) {
-//            number = String.valueOf(array[i]);
-//            if (Character.isDigit(text))
-//            System.out.println(array[i]);
-//        }
-//    }
-//}
+public class Task1_3{
+    public void printInts(String text) {
+        char[] array = text.toCharArray();
+        String number;
+        for (int i = 0; i < array.length; i++) {
+            number = String.valueOf(array[i]);
+            if (Character.isDigit(array))
+            System.out.println(array[i]);
+        }
+    }
+}
+
 
