e76a83662477f317f841c35ddcefc9c18a4323ce
Uladzimir Hubski <Uladzimir_Hubski@epam.com>
2023-03-20 18:42:25 +0300

    Hometask Playground essence 7 part

---
+++ src/homework/day6/GenericMethodsInGenericClassTRunner.java
@@ -0,0 +1,12 @@
+package homework.day6;
+
+public class GenericMethodsInGenericClassTRunner {
+    public static void main(String[] args) {
+
+
+        GenericMethodsInGenericClassT<Integer> integerGen = new GenericMethodsInGenericClassT<>();
+        GenericMethodsInGenericClassT<String> stringGen = new GenericMethodsInGenericClassT<>();
+        GenericMethodsInGenericClassT<Double> doubleGen = new GenericMethodsInGenericClassT<>();
+//        tbd
+   }
+}
