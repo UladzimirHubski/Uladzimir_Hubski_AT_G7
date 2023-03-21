f712d9fc1752e5153089a484f07ce34556b0ad4e
Uladzimir_Hubski <vladimirgubskij@gmail.com>
2023-03-21 16:52:40 +0300

    Complete task 6th

--- src/homework/day8/Task6.java
+++ src/homework/day8/Task6.java
@@ -7,26 +7,26 @@ public class Task6 {
     public static void main(String[] args) {
         String[] bird = {"Чайка", "Дрозд", "Бусел", "Голубь", "Воробей", "Цапля"};
         List<String> birds = Arrays.asList(bird);
-//        int count = 0;
+        int count = 0;
 
         for (String bird1 : birds) {
             System.out.print("--" + bird1 + "--\n");
         }
 
-//        for (String syllableCount : birds) {
-//            if (syllableCount.matches("['а', 'я', 'у', 'ю', 'о', 'е', 'ё', 'э', 'и', 'ы']")) {
-//                count++;
-//            }
-//        }
-//        System.out.print(count + "\n");
-
-            for (int i = 0; i < birds.size(); i++) {
-                System.out.print(i + ":" + birds.get(i) + " ");
+        for (String syllableCount : birds) {
+            if (syllableCount.matches("((.*)[аяуюоеёэиы](.*)){2,}")) {
+                count++;
             }
+        }
+        System.out.print(count + "\n");
+
+        for (int i = 0; i < birds.size(); i++) {
+            System.out.print(i + ":" + birds.get(i) + " ");
+        }
 
         birds.set(3, "Синица");
 
-        System.out.println("\n");
+        System.out.print("\n");
 
         for (String bird1 : birds) {
             System.out.print(bird1 + " ");
